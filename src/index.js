ec80e6780b811858cd16661c7d65cd7ba7b13902
Colin White <clwhite814@gmail.com>
2018-11-20 13:50:33 -0600

    data is console loggins

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
-import loadProjects from './projects';
+import loadProjects from './helpers/projects';
 
 const initializeApp = () => {
   $('#tempdiv').html('<h1>This is a test</h1>');
