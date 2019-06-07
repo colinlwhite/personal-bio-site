099ba556c23961b31ecadb33c62c0437a3547cb6
Colin White <clwhite814@gmail.com>
2019-06-06 21:47:56 -0500

    navbar is in front of the video

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,16 +4,16 @@ import email from '../../images/email.svg';
 import linkedin from '../../images/linkedin.png';
 import githublack from '../../images/gitblack.svg';
 
-const footerStuff = () => {
-  const domString = `
-    <a class="navbar-brand">&copy; 2019 Colin White</a>
-    <a href="mailto:clwhite814@gmail.com"><img id="footer-image" src="${email}" alt="Email"></a>
-    <a href="https://www.linkedin.com/in/colinlwhite/" target="_blank"><img id="footer-image" src="${linkedin}" alt="LinkedIn"></a>
-    <a href="https://github.com/colinlwhite" target="_blank"><img id="footer-image" src="${githublack}" alt="GitHub"></a>
-  <!-- </nav> -->
-</footer>
-  `;
-  $('.page-footer').html(domString);
-};
+// const footerStuff = () => {
+//   const domString = `
+//     <a class="navbar-brand">&copy; 2019 Colin White</a>
+//     <a href="mailto:clwhite814@gmail.com"><img id="footer-image" src="${email}" alt="Email"></a>
+//     <a href="https://www.linkedin.com/in/colinlwhite/" target="_blank"><img id="footer-image" src="${linkedin}" alt="LinkedIn"></a>
+//     <a href="https://github.com/colinlwhite" target="_blank"><img id="footer-image" src="${githublack}" alt="GitHub"></a>
+//   <!-- </nav> -->
+// </footer>
+//   `;
+//   $('.page-footer').html(domString);
+// };
 
-export default footerStuff;
+// export default footerStuff;
