1ae6e2dc822d8cc8e0857b75c28e10b94cd835a7
Colin White <clwhite814@gmail.com>
2019-07-20 15:02:58 -0500

    one small step for man, one giant leap for mankind

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,6 +3,32 @@ import './home.scss';
 // import vid from './LiftOff.mov';
 import vid from './Rocket.mp4';
 
+const homePage = () => {
+  if ($(window).width() >= 961) {
+    const domString = `
+      <header class="v-header container">
+      <div class="fullscreen-video-wrap">
+    <video src="${vid}" autoplay="true" loop="true" muted="muted"></video>
+      </div>
+      <div class="header-overlay">
+      <div>
+      <h1 class="home-page-header">console.log('Colin White: ', developer + ' and ' + Musician);</h1>
+      </div>
+      </div>
+      </header>
+      `;
+    $('#homeDiv').html(domString);
+  } else {
+    const domString = `
+     <h1 id="home-h1">console.log('Colin White: ', developer + ' and ' + Musician);</h1>
+     `;
+    $('#homeDiv').html(domString);
+  }
+};
+
+
+export default homePage;
+
 // const homePage = () => {
 //   const domString = `
 //   <h1 id="home-h1">console.log('Colin White: ', developer + ' and ' + Musician);</h1>
@@ -13,20 +39,20 @@ import vid from './Rocket.mp4';
 // export default homePage;
 
 
-const homePage = () => {
-  const domString = `
-  <header class="v-header container">
-  <div class="fullscreen-video-wrap">
-<video src="${vid}" autoplay="true" loop="true" muted="muted"></video>
-  </div>
-  <div class="header-overlay">
-  <div>
-  <h1 class="home-page-header">console.log('Colin White: ', developer + ' and ' + Musician);</h1>
-  </div>
-  </div>
-  </header>
-  `;
-  $('#homeDiv').html(domString);
-};
+// const homePage = () => {
+//   const domString = `
+//   <header class="v-header container">
+//   <div class="fullscreen-video-wrap">
+// <video src="${vid}" autoplay="true" loop="true" muted="muted"></video>
+//   </div>
+//   <div class="header-overlay">
+//   <div>
+//   <h1 class="home-page-header">console.log('Colin White: ', developer + ' and ' + Musician);</h1>
+//   </div>
+//   </div>
+//   </header>
+//   `;
+//   $('#homeDiv').html(domString);
+// };
 
-export default homePage;
+// export default homePage;
