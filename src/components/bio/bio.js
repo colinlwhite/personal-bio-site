0ded96c35e99035db25c295e707d459fa8400951
Colin White <clwhite814@gmail.com>
2019-07-21 18:36:24 -0500

    added new photo and rounded off the image

--- src/components/bio/bio.js
+++ src/components/bio/bio.js
@@ -1,12 +1,14 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './bio.scss';
-import photo from '../../images/cw.jpg';
+import photo from '../../images/e8headshot.jpg';
 
 const bioPage = () => {
   const bioString = `
   <h1 id="bio-header">BIO</h1>
-  <img id="bio-photo" src="${photo}" class="rounded img-fluid d-block mx-auto" alt="Colin White">
+  <div>
+  <img id="bio-photo" src="${photo}" class="img-fluid d-block mx-auto" alt="Colin White">
+  </div>
   <div id="bioDiv">
   <p>Full Stack Developer transitioning from the music instruments industry into technology.</p>
     <p>I revel that, as Alan Kay once said, "Technology is anything that wasn't around when you were born."</p>
