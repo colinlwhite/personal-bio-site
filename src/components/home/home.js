4951cd7a2eaa7e14e5054c6cdc37d0107edf74bc
Colin White <clwhite814@gmail.com>
2019-07-21 16:11:50 -0500

    wip

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
-import './home.scss';
-// import vid from './LiftOff.mov';
 import vid from './Rocket.mp4';
+import './home.scss';
 
 const homePage = () => {
   if ($(window).width() >= 1024) {
@@ -12,7 +11,7 @@ const homePage = () => {
       </div>
       <div class="header-overlay">
       <div>
-      <h1 class="home-page-header">console.log('Colin White: ', developer + ' and ' + Musician);</h1>
+      <h1 class="home-page-header">Colin White</h1>
       </div>
       </div>
       </header>
@@ -20,41 +19,10 @@ const homePage = () => {
     $('#homeDiv').html(domString);
   } else {
     const domString = `
-    <div class="mobile-div">
-     <h1 id="home-h1">console.log('Colin White: ', developer + ' and ' + Musician);</h1>
-     </div>
+     <h1 id="home-h1">Colin White</h1>
      `;
     $('#homeDiv').html(domString);
   }
 };
 
-
 export default homePage;
-
-// const homePage = () => {
-//   const domString = `
-//   <h1 id="home-h1">console.log('Colin White: ', developer + ' and ' + Musician);</h1>
-//   `;
-//   $('#homeDiv').html(domString);
-// };
-
-// export default homePage;
-
-
-// const homePage = () => {
-//   const domString = `
-//   <header class="v-header container">
-//   <div class="fullscreen-video-wrap">
-// <video src="${vid}" autoplay="true" loop="true" muted="muted"></video>
-//   </div>
-//   <div class="header-overlay">
-//   <div>
-//   <h1 class="home-page-header">console.log('Colin White: ', developer + ' and ' + Musician);</h1>
-//   </div>
-//   </div>
-//   </header>
-//   `;
-//   $('#homeDiv').html(domString);
-// };
-
-// export default homePage;
