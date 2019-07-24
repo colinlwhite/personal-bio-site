4c127dcd4ef1be29d0c71569cbea6159cfa00d83
Colin White <clwhite814@gmail.com>
2019-07-24 08:01:33 -0500

    wip

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,10 +2,33 @@ import $ from 'jquery';
 import vid from './Rocket.mp4';
 import './home.scss';
 
+// const homePage = () => {
+//   $(document).ready(() => {
+//     if ($(window).width() >= 1024) {
+//       const domString = `
+//        <header class="v-header container">
+//             <div class="fullscreen-video-wrap">
+//                 <video src="${vid}" autoplay="true" loop="true" muted="muted"></video>
+//             </div>
+//             <div class="header-overlay">
+//             <div>
+//                 <h1 class="home-page-header">COLIN WHITE</h1>
+//             </div>
+//             </div>
+//        </header>
+//        `;
+//       $('#homeDiv').html(domString);
+//     } else {
+//       const domString = `
+
+//       `;
+//       $('#homeDiv').html(domString);
+//     }
+//   });
+// };
+
 const homePage = () => {
-  $(document).ready(() => {
-    if ($(window).width() >= 1024) {
-      const domString = `
+  const domString = `
        <header class="v-header container">
             <div class="fullscreen-video-wrap">
                 <video src="${vid}" autoplay="true" loop="true" muted="muted"></video>
@@ -17,14 +40,8 @@ const homePage = () => {
             </div>
        </header>
        `;
-      $('#homeDiv').html(domString);
-    } else {
-      const domString = `
-  
-      `;
-      $('#homeDiv').html(domString);
-    }
-  });
+  $('#homeDiv').html(domString);
 };
+
 
 export default homePage;
