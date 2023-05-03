5324dfa7388ce7fb488c1fece704e0dd5910d351
Colin White <clwhite814@gmail.com>
2023-05-02 19:28:48 -0500

    removed eslint-loader and installed eslint-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,8 @@ const extractScss = new ExtractTextPlugin({
   disable: isDev
 });
 
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
   devtool: 'source-map',
   entry: {
@@ -25,45 +27,46 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html')
     }),
-    extractScss
+    extractScss,
+    new ESLintPlugin()
   ],
   module: {
-    rules: [{
-      enforce: 'pre', // Happens before all the other tasks
-      test: /\.js$/, // Run eslint on all files with an extension of .js
-      exclude: /node_modules/, // That are not in the node_modules folder
-      use: 'eslint-loader' // It's using the eslint-loader package
-    }, 
-    // {
-    //   test: /\.mp4$/,
-    //   exclude: /node_modules/,
-    //   use: 'file-loader'
-    // },
-    {
-      test: /\.(png|jp(e*)g|svg|mov|mp4)$/,  
-      use: [{
-          loader: 'file-loader',
-          options: { 
+    rules: [
+      // {
+      //   test: /\.mp4$/,
+      //   exclude: /node_modules/,
+      //   use: 'file-loader'
+      // },
+      {
+        test: /\.(png|jp(e*)g|svg|mov|mp4)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
               limit: 8000, // Convert images < 8kb to base64 strings
               // name: 'images/[hash]-[name].[ext]'
-          } 
-      }]
-    },
-    {
-      test: /\.js$/, // converts all files that w/.js from ES6 to ES5 so old browsers like IE can run our code.
-      exclude: /node_modules/, // That are not in the node_modules folder.
-      use: 'babel-loader'
-    }, {
-      test: /(\.css|\.scss)$/, // grabbing all files that end in this
-      exclude: /node_modules/, // except in this folder
-      use: extractScss.extract({ // extract scss plugin for css-loader package
-        use:[
-          {loader: 'css-loader'}, // copying the new CSS files to the build folder
-          {loader: 'sass-loader'} // converting .scss to css
-        ],
-        fallback: 'style-loader'
-      })
-    }]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/, // converts all files that w/.js from ES6 to ES5 so old browsers like IE can run our code.
+        exclude: /node_modules/, // That are not in the node_modules folder.
+        use: 'babel-loader'
+      },
+      {
+        test: /(\.css|\.scss)$/, // grabbing all files that end in this
+        exclude: /node_modules/, // except in this folder
+        use: extractScss.extract({
+          // extract scss plugin for css-loader package
+          use: [
+            { loader: 'css-loader' }, // copying the new CSS files to the build folder
+            { loader: 'sass-loader' } // converting .scss to css
+          ],
+          fallback: 'style-loader'
+        })
+      }
+    ]
   },
   devServer: {
     historyApiFallback: true,
