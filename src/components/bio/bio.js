5606e0ca74824bafacf10c2d756cdffa99a97aaa
Colin White <clwhite814@gmail.com>
2024-07-24 06:41:06 -0500

    fixed code formatting, updated the image and made the image a circle

--- src/components/bio/bio.js
+++ src/components/bio/bio.js
@@ -1,19 +1,19 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './bio.scss';
-import photo from '../../images/e8headshot.jpg';
+import bioPhoto from '../../images/profilepicture.jpg';
 
 const bioPage = () => {
   const bioString = `
-  <div class="container d-flex flex-column justify-content-center p-5 mt-4">
-  <div>
-  <img id="bio-photo" src="${photo}" class="img-fluid d-block mx-auto" alt="Colin White">
-  </div>
-  <div id="bioDiv"> 
-    <p>I am a Software Engineer with two years of experience in the fintech industry. 
-    I am proficient in C#, Angular, SQL, and Kubernetes. 
-    I am a continuous learner who is always striving to improve my skills and add value to the organization I serve.</>
-    </div>
+    <div class="container d-flex flex-column justify-content-center p-5 mt-4">
+      <div>
+        <img id="bio-photo" src="${bioPhoto}" class="img-fluid d-block mx-auto" alt="Colin White">
+      </div>
+      <div id="bioDiv"> 
+        <p>I am a Software Engineer with two years of experience in the fintech industry. 
+        I am proficient in C#, Angular, SQL, and Kubernetes. 
+        I am a continuous learner who is always striving to improve my skills and add value to the organization I serve.</p>
+      </div>
     </div>
   `;
   $('#bioPage').html(bioString);
