f7fb09d189c4797ecd0845b7deef5e6acab198fd
Colin White <clwhite814@gmail.com>
2024-08-28 18:48:02 -0500

    WIP: text-spinner is working, but work still needed

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -13,7 +13,7 @@ const homePage = () => {
           <div class="header-overlay">
             <div>
               <h1 class="home-page-header">COLIN L. WHITE</h1>
-              <h2 class="home-page-header">SOFTWARE ENGINEER</h2>
+              <h2 class="home-page-header" id="spinning-wheel">SOFTWARE ENGINEER</h2>
             </div>
           </div>
         </header>
@@ -25,6 +25,48 @@ const homePage = () => {
       `;
       $('#homeDiv').html(homePageHtml);
     }
+
+    // Spinning Wheel
+
+    // document.addEventListener("DOMContentLoaded", function() {
+    const words = ['Son', 'Husband', 'Father', 'Brother', 'Uncle', 'Drummer', 'REI', 'Software Engineer'];
+    const spinningWheel = document.getElementById('spinning-wheel');
+
+    let wordIndex = 0;
+    let interval;
+    let speed = 50; // Initial speed (milliseconds)
+
+    function spin() {
+      spinningWheel.textContent = words[wordIndex];
+      wordIndex = (wordIndex + 1) % words.length;
+    }
+
+    function startSpinning() {
+      interval = setInterval(spin, speed);
+    }
+
+    function stopSpinning() {
+      clearInterval(interval);
+      setTimeout(() => {
+        spinningWheel.textContent = 'Software Engineer';
+      }, 2000); // 2-second delay before displaying the final word
+    }
+
+    // Start spinning at a fast pace
+    startSpinning();
+
+    // Gradually slow down the spinning
+    setTimeout(() => {
+      clearInterval(interval); speed = 100; startSpinning();
+    }, 1000); // Slow down after 1 second
+    setTimeout(() => {
+      clearInterval(interval); speed = 200; startSpinning();
+    }, 2000); // Slow down after 2 seconds
+    setTimeout(() => {
+      clearInterval(interval); speed = 400; startSpinning();
+    }, 3000); // Slow down after 3 seconds
+    setTimeout(stopSpinning, 4000); // Stop after 4 seconds
+    // });
   });
 };
 
